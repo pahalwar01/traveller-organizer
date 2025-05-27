0e7343c21bf51c064edae85107d57963e192d60f
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2025-05-28 00:06:59 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,1 +1,10 @@
-<?php session_start(); session_destroy(); header('Location: login.php'); ?>
+<?php
+session_start();
+
+// Destroy all session data
+session_unset();
+session_destroy();
+
+// Redirect to login page
+header("Location: login.php");
+exit();
