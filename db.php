32b813ddbbbff345aae57432ad472ab4f45342fb
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2025-05-28 00:06:51 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,1 +1,6 @@
-<?php $conn = new mysqli('localhost', 'root', '', 'traveller_db'); ?>
+<?php
+$conn = new mysqli("localhost", "root", "", "traveller_db");
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
